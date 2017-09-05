1598b4777f3ffe0dadd675213e148dea0c53601e
Brostep <ltr.pereyra@gmail.com>
2017-09-05 17:12:59 -0300

    Create player movement, and color

---
+++ GameTest/Assets/Scritps/playerMovement.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+	//lala
+	Rigidbody rb;
+	// testGit
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+}
